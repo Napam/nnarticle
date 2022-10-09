bdb48fb5b03b1b5c903593b9567c87cd8c5621f1
Naphat Amundsen <naphat@live.no>
2022-10-09 22:28:32 +0200

    change strengths to likelihoods

--- figures/compress.sh
+++ figures/compress.sh
@@ -1,7 +1,7 @@
 # Used for compressing gifs
 
 compress () {
-    gifsicle -v -i $1 -O3 --colors 32 --lossy=20 -o ${1%%.*}_compressed.gif
+    gifsicle -v -i $1 -O3 --colors 64 --lossy=20 -o ${1%%.*}_compressed.gif
 }
 
 compress $1
