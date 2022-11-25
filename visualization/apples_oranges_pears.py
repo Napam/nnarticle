0bc33d9c0bb84ee698a94e3e1e0a0cfff23d9cd3
Naphat Amundsen <naphat@live.no>
2022-11-25 22:41:19 +0100

    3lp animation refactored

--- visualization/apples_oranges_pears.py
+++ visualization/apples_oranges_pears.py
@@ -43,14 +43,10 @@ m = X.mean(0)
 s = X.std(0)
 
 model_weights_2lp = json_to_weights(project_dir / "models" / "weights" / "2LP.json")
-u2lp_biases, u2lp_weights = unnormalize_planes(
-    m, s, model_weights_2lp["planes.bias"], model_weights_2lp["planes.weight"]
-)
+u2lp_biases, u2lp_weights = unnormalize_planes(m, s, model_weights_2lp["planes.bias"], model_weights_2lp["planes.weight"])
 
 model_weights_3lp = json_to_weights(project_dir / "models" / "weights" / "3LP.json")
-uhidden_biases, uhidden_weights = unnormalize_planes(
-    m, s, model_weights_3lp["hidden.bias"], model_weights_3lp["hidden.weight"]
-)
+uhidden_biases, uhidden_weights = unnormalize_planes(m, s, model_weights_3lp["hidden.bias"], model_weights_3lp["hidden.weight"])
 output_biases, output_weights = model_weights_3lp["output.bias"], model_weights_3lp["output.weight"]
 
 
@@ -155,9 +151,7 @@ def visualize_data_with_hidden_lines(ax: plt.Axes = None, scatter_kwargs: dict =
     artists = {}
     artists["lines"] = lines = []
     for (color, bias, weights) in zip(colors[[0, 2]], uhidden_biases, uhidden_weights):
-        _, artists_ = plot_hyperplane(
-            xspace, bias, *weights, 10, c=color, quiver_kwargs=quiver_kwargs, ax=ax, return_artists=True
-        )
+        _, artists_ = plot_hyperplane(xspace, bias, *weights, 10, c=color, quiver_kwargs=quiver_kwargs, ax=ax, return_artists=True)
         lines.append(artists_["line"][0])
 
     # plt.show()
@@ -261,21 +255,11 @@ def visualize_2lp_activations(point: np.ndarray = None, axes: tuple[plt.Axes, pl
     ax_lower.annotate("Orangeness", ccenters[1][2] + [radius + 0.1, 0], ha="left", va="center", fontsize=fontsize)
 
     # Text inside nodes
-    artists["x_text"] = ax_lower.annotate(
-        f"{point[0, 0]:.1f}", ccenters[0][0], ha="center", va="center", fontsize=fontsize
-    )
-    artists["y_text"] = ax_lower.annotate(
-        f"{point[0, 1]:.1f}", ccenters[0][1], ha="center", va="center", fontsize=fontsize
-    )
-    artists["out0_text"] = ax_lower.annotate(
-        f"{activations[0]:.2f}", ccenters[1][0], ha="center", va="center", fontsize=fontsize
-    )
-    artists["out1_text"] = ax_lower.annotate(
-        f"{activations[1]:.2f}", ccenters[1][1], ha="center", va="center", fontsize=fontsize
-    )
-    artists["out2_text"] = ax_lower.annotate(
-        f"{activations[2]:.2f}", ccenters[1][2], ha="center", va="center", fontsize=fontsize
-    )
+    artists["x_text"] = ax_lower.annotate(f"{point[0, 0]:.1f}", ccenters[0][0], ha="center", va="center", fontsize=fontsize)
+    artists["y_text"] = ax_lower.annotate(f"{point[0, 1]:.1f}", ccenters[0][1], ha="center", va="center", fontsize=fontsize)
+    artists["out0_text"] = ax_lower.annotate(f"{activations[0]:.2f}", ccenters[1][0], ha="center", va="center", fontsize=fontsize)
+    artists["out1_text"] = ax_lower.annotate(f"{activations[1]:.2f}", ccenters[1][1], ha="center", va="center", fontsize=fontsize)
+    artists["out2_text"] = ax_lower.annotate(f"{activations[2]:.2f}", ccenters[1][2], ha="center", va="center", fontsize=fontsize)
 
     # Current class node
     curr_class = np.argmax(activations)
@@ -373,9 +357,7 @@ def visualize_appleness_pearness(axes: tuple[plt.Axes, plt.Axes] = None, scatter
 
     ax_right.scatter(*h1[y == 0].T, label="Apple", marker="^", c="greenyellow", edgecolor="black", **scatter_kwargs)
     ax_right.scatter(*h1[y == 1].T, label="Orange", marker="o", c="orange", edgecolor="black", **scatter_kwargs)
-    ax_right.scatter(
-        *h1[y == 2].T, label="Pear", marker="s", c="forestgreen", edgecolor="black", s=20, **scatter_kwargs
-    )
+    ax_right.scatter(*h1[y == 2].T, label="Pear", marker="s", c="forestgreen", edgecolor="black", s=20, **scatter_kwargs)
     ax_right.set_xlabel("Appleness")
     ax_right.set_ylabel("Pearness")
     ax_right.set_title("Activation space")
@@ -426,20 +408,18 @@ def visualize_appleness_pearness_out_lines(axes: tuple[plt.Axes, plt.Axes] = Non
 
 @viz_decorator("3lp.pdf")
 def visualize_3lp(point: np.ndarray = None):
-
     fig = plt.figure(figsize=(10, 8))
     ax_upperleft = fig.add_subplot(221)
     ax_upperright = fig.add_subplot(222)
     ax_bottom = fig.add_subplot(212)
 
-    _, _, artists = visualize_appleness_pearness_out_lines(
-        False, False, axes=(ax_upperleft, ax_upperright), scatter_kwargs={"alpha": 0.25}
-    )
+    _, _, artists = visualize_appleness_pearness_out_lines(False, False, axes=(ax_upperleft, ax_upperright), scatter_kwargs={"alpha": 0.25})
     # Artist keys: hidden_lines, out_lines
 
     point0 = np.array([point or [140, 10]], dtype=float)
     h1 = forward_sigmoid(point0, uhidden_biases, uhidden_weights)[0]
     point1 = h1
+    h2 = forward_sigmoid(point1, output_biases, output_weights)
 
     # "Unknown" scatter points
     artists["point0"] = ax_upperleft.scatter(*point0[None].T, label="Unknown", **unknown_point_kwargs, zorder=100)
@@ -448,8 +428,6 @@ def visualize_3lp(point: np.ndarray = None):
     # Lines with varying widths
     for i, hidden_line in enumerate(artists["hidden_lines"]):
         hidden_line.set_linewidth(calc_linewidth(h1[i]))
-
-    h2 = forward_sigmoid(point1, output_biases, output_weights)
 
     for i, out_line in enumerate(artists["out_lines"]):
         out_line.set_linewidth(calc_linewidth(h2[i]))
@@ -468,12 +446,12 @@ def visualize_3lp(point: np.ndarray = None):
         circle_kwargs=circle_kwargs,
         quiver_kwargs={"color": "k", "width": 0.016},
     )
-    artists["circles"] = list(chain.from_iterable(circles[1:]))
+    artists["circles"] = circles[1:]
     ccenters = [[np.array(circle.get_center()) for circle in layer] for layer in circles]
 
     # Hidden node colors
     circles[1][0].set_facecolor(colors[0])
-    circles[1][1].set_facecolor(colors[1])
+    circles[1][1].set_facecolor(colors[2])
 
     # Output node colors
     circles[2][0].set_facecolor(colors[0])
@@ -540,252 +518,85 @@ def visualize_3lp(point: np.ndarray = None):
     ax_bottom.set_yticks([])
     ax_bottom.axis("off")
 
-    plt.show()
+    fig.tight_layout()
+
+    # plt.show()
+    return fig, (ax_upperleft, ax_upperright, ax_bottom), artists
 
 
 def visualize_3lp_animated():
-    hidden_biases = np.array(
-        [
-            -1.9472151,  # Apple
-            -2.260901,  # Pear
-        ]
-    )
+    fig, (ax_upperleft, ax_upperright, ax_bottom), artists = visualize_3lp(False, False)
 
-    hidden_weights = np.array(
-        [
-            [-4.1687274, -1.3713175],
-            [4.5323997, -1.6058096],
-        ]
-    )
+    hidden_lines = artists["hidden_lines"]
+    out_lines = artists["out_lines"]
+    scatterpoint0 = artists["point0"]
+    scatterpoint1 = artists["point1"]
+    circles = artists["circles"]
+    x_text = artists["x_text"]
+    y_text = artists["y_text"]
+    hidden1_text = artists["hidden1_text"]
+    hidden2_text = artists["hidden2_text"]
+    out1_text = artists["out1_text"]
+    out2_text = artists["out2_text"]
+    out3_text = artists["out3_text"]
+    class_text = artists["class_text"]
+    class_circle = artists["class_circle"]
 
-    output_biases = np.array(
-        [
-            -2.0450604,  # Apple
-            -2.1543744,  # Pear
-            2.6014535,  # Orange
-        ]
-    )
-
-    output_weights = np.array(
-        [
-            [5.4452653, -1.87916],
-            [-2.0285792, 5.59163],
-            [-4.693778, -5.0045652],
-        ]
-    )
-
-    m = np.array([141.8463, 6.2363])
-    s = np.array([10.5088, 1.7896])
-
-    uhidden_biases, uhidden_weights = unnormalize_planes(m, s, hidden_biases, hidden_weights)
-
-    plot_kwargs = {}
-    quiver_kwargs = {"units": "dots", "width": 2, "headwidth": 8, "scale": 0.075, "scale_units": "dots"}
-
-    classes = ["Apple", "Pear", "Orange"]
-    labels = ["Apple boundary", "Pear boundary", "Orange boundary"]
-    colors = ["greenyellow", "forestgreen", "orange"]
-    linestyles = [None, None, None]
-
-    fig = plt.figure(figsize=(10, 8))
-    ax_upperleft = fig.add_subplot(221)
-    ax_upperright = fig.add_subplot(222)
-    ax_bottom = fig.add_subplot(212)
-
-    xspace = np.linspace(*x_lim, 4)
-    ax_upperleft.scatter(*X[y == 0].T, label="Apple", marker="^", c="greenyellow", edgecolor="black", alpha=0.25)
-    ax_upperleft.scatter(*X[y == 1].T, label="Orange", marker="o", c="orange", edgecolor="black", alpha=0.25)
-    ax_upperleft.scatter(*X[y == 2].T, label="Pear", marker="s", c="forestgreen", edgecolor="black", s=20, alpha=0.25)
-
-    # For use in FuncAnimation
-    animated_artists = []
-
-    hidden_lines = []
-    for i, (label, color, linestyle) in enumerate(zip(labels[:2], colors[:2], linestyles[:2])):
-        _, artists = plot_hyperplane(
-            xspace,
-            uhidden_biases[i],
-            *uhidden_weights[i],
-            5,
-            c=color,
-            plot_kwargs={**plot_kwargs, "label": label, "linestyle": linestyle},
-            quiver_kwargs=quiver_kwargs,
-            ax=ax_upperleft,
-            return_artists=True,
-        )
-        hidden_lines.append(artists["line"][0])
-
-    animated_artists.extend(hidden_lines)
-
-    point1 = np.array([[0, 0]], dtype=float)
-    animated_artists.append(
-        scatterpoint1 := ax_upperleft.scatter(*point1.T, label="Unknown", marker="x", c="black", s=70, zorder=100)
-    )
-
-    h1 = uhidden_biases + X @ uhidden_weights.T
-    h1 = 1 / (1 + np.exp(-h1))
-
-    point2 = np.array([[0, 0]], dtype=float)
-    animated_artists.append(
-        scatterpoint2 := ax_upperright.scatter(*point2.T, label="Unknown", marker="x", c="black", s=70, zorder=100)
-    )
-    ax_upperright.scatter(*h1[y == 0].T, label="Apple", marker="^", c="greenyellow", edgecolor="black", alpha=0.25)
-    ax_upperright.scatter(*h1[y == 1].T, label="Orange", marker="o", c="orange", edgecolor="black", alpha=0.25)
-    ax_upperright.scatter(*h1[y == 2].T, label="Pear", marker="s", c="forestgreen", edgecolor="black", s=20, alpha=0.25)
-
-    outlims = get_lims(h1)
-    xspace2 = np.linspace(*outlims[0], 10)
-    output_lines = []
-    for i, (label, color, linestyle) in enumerate(zip(labels, colors, linestyles)):
-        _, artists = plot_hyperplane(
-            xspace2,
-            output_biases[i],
-            *output_weights[i],
-            5,
-            c=color,
-            plot_kwargs={**plot_kwargs, "label": label, "linestyle": linestyle},
-            quiver_kwargs=quiver_kwargs,
-            ax=ax_upperright,
-            return_artists=True,
-        )
-        output_lines.append(artists["line"][0])
-
-    animated_artists.extend(output_lines)
-
-    radius = 0.25
-    ann_center = (-1.3, 0)
-    fontsize = 13
-    circle_kwargs = {"facecolor": (0, 0, 0, 0), "edgecolor": "k"}
-    circles = draw_ann(
-        layers=[2, 2, 3],
-        radius=radius,
-        center=ann_center,
-        spacing=(0.5, 0.4),
-        ax=ax_bottom,
-        circle_kwargs=circle_kwargs,
-        quiver_kwargs={"color": "k", "width": 0.016},
-    )
-    animated_artists.extend(chain.from_iterable(circles[1:]))
-    ccenters = [[np.array(circle.get_center()) for circle in layer] for layer in circles]
-
-    # Hidden node colors
-    circles[1][0].set_facecolor(colors[0])
-    circles[1][1].set_facecolor(colors[1])
-
-    # Output node colors
-    circles[2][0].set_facecolor(colors[0])
-    circles[2][1].set_facecolor(colors[1])
-    circles[2][2].set_facecolor(colors[2])
-
-    # Static text in ax_bottom
-    ax_bottom.annotate("Weight", ccenters[0][0] - [radius + 0.1, 0], ha="right", va="center", fontsize=fontsize)
-    ax_bottom.annotate("Diameter", ccenters[0][1] - [radius + 0.1, 0], ha="right", va="center", fontsize=fontsize)
-    ax_bottom.annotate("Model graph", ccenters[1][0] + [0, 0.6], ha="center", va="center", fontsize=14)
-    ax_bottom.annotate("Appleness", ccenters[2][0] + [radius + 0.1, 0], ha="left", va="center", fontsize=fontsize)
-    ax_bottom.annotate("Pearness", ccenters[2][1] + [radius + 0.1, 0], ha="left", va="center", fontsize=fontsize)
-    ax_bottom.annotate("Orangeness", ccenters[2][2] + [radius + 0.1, 0], ha="left", va="center", fontsize=fontsize)
-
-    # Text inside nodes
-    animated_artists.extend(
-        [
-            x_text := ax_bottom.annotate("x", ccenters[0][0], ha="center", va="center", fontsize=fontsize),
-            y_text := ax_bottom.annotate("y", ccenters[0][1], ha="center", va="center", fontsize=fontsize),
-            hidden1_text := ax_bottom.annotate("h1", ccenters[1][0], ha="center", va="center", fontsize=fontsize),
-            hidden2_text := ax_bottom.annotate("h2", ccenters[1][1], ha="center", va="center", fontsize=fontsize),
-            out1_text := ax_bottom.annotate("o1", ccenters[2][0], ha="center", va="center", fontsize=fontsize),
-            out2_text := ax_bottom.annotate("o2", ccenters[2][1], ha="center", va="center", fontsize=fontsize),
-            out3_text := ax_bottom.annotate("o3", ccenters[2][2], ha="center", va="center", fontsize=fontsize),
-        ]
-    )
-
-    # Current class node
-    class_ccenter = np.array([1.6, 0])
-    class_cradius = radius * 1.75
-    ax_bottom.annotate(
-        "Current classification",
-        class_ccenter + [0, class_cradius + 0.2],
-        ha="center",
-        va="center",
-        fontsize=14,
-    )
-    animated_artists.append(class_circle := patches.Circle(class_ccenter, radius=class_cradius, **circle_kwargs))
-    animated_artists.append(
-        class_text := ax_bottom.annotate("Apple", class_ccenter, ha="center", va="center", fontsize=14)
-    )
-    ax_bottom.add_patch(class_circle)
-
-    n = 300  # Animation steps
-    pi2 = np.pi * 2
-    max_linewidth = 9
-    min_linewidth = 0.5
+    n = 300
     pbar = tqdm(total=n + 1, disable=False)
     centerx = np.mean(x_lim)
     centery = np.mean(y_lim)
 
+    point0 = np.array([[0, 0]], dtype=float)
+
+    artists_to_animate = pd.core.common.flatten(artists.values())
+    pi2 = np.pi * 2
+
     def step(i):
         rad = i / n * pi2
-        point1[0, 0] = centerx + 20 * math.cos(rad)
-        point1[0, 1] = centery + 5 * math.sin(rad)
+        point0[0, 0] = centerx + 20 * math.cos(rad)
+        point0[0, 1] = centery + 5 * math.sin(rad)
+
+        h1 = forward_sigmoid(point0, uhidden_biases, uhidden_weights)[0]
+        point1 = h1
+        h2 = forward_sigmoid(point1, output_biases, output_weights)
+
+        scatterpoint0.set_offsets(point0)
         scatterpoint1.set_offsets(point1)
-        x_text.set_text(f"{point1[0, 0]:.1f}")
-        y_text.set_text(f"{point1[0, 1]:.1f}")
 
-        # Hidden line widths
-        h1_ = uhidden_biases + point1 @ uhidden_weights.T
-        h1_ = 1 / (1 + np.exp(-h1_))
-        for i, line in enumerate(hidden_lines):
-            line.set_linewidth(max(h1_[0, i] * max_linewidth, min_linewidth))
+        # Lines with varying widths
+        for i, hidden_line in enumerate(hidden_lines):
+            hidden_line.set_linewidth(calc_linewidth(h1[i]))
 
-        hidden1_text.set_text(f"{h1_[0, 0]:.2f}")
-        hidden2_text.set_text(f"{h1_[0, 1]:.2f}")
-        circles[1][0].set_facecolor((*circles[1][0].get_facecolor()[:3], h1_[0, 0]))
-        circles[1][1].set_facecolor((*circles[1][1].get_facecolor()[:3], h1_[0, 1]))
+        for i, out_line in enumerate(out_lines):
+            out_line.set_linewidth(calc_linewidth(h2[i]))
 
-        scatterpoint2.set_offsets(h1_)
+        # Hidden node colors
+        circles[0][0].set_facecolor((*colors[0], h1[0]))
+        circles[0][1].set_facecolor((*colors[2], h1[1]))
 
-        # Output line widths
-        out_ = output_biases + h1_ @ output_weights.T
-        out_ = 1 / (1 + np.exp(-out_))
-        for i, line in enumerate(output_lines):
-            line.set_linewidth(max(out_[0][i] * max_linewidth, min_linewidth))
+        # Output node colors
+        circles[1][0].set_facecolor((*colors[0], h2[0]))
+        circles[1][1].set_facecolor((*colors[1], h2[1]))
+        circles[1][2].set_facecolor((*colors[2], h2[2]))
 
-        out1_text.set_text(f"{out_[0, 0]:.2f}")
-        out2_text.set_text(f"{out_[0, 1]:.2f}")
-        out3_text.set_text(f"{out_[0, 2]:.2f}")
-        circles[2][0].set_facecolor((*circles[2][0].get_facecolor()[:3], out_[0, 0]))
-        circles[2][1].set_facecolor((*circles[2][1].get_facecolor()[:3], out_[0, 1]))
-        circles[2][2].set_facecolor((*circles[2][2].get_facecolor()[:3], out_[0, 2]))
+        # Text stuff
+        x_text.set_text(f"{point0[0, 0]:.1f}")
+        y_text.set_text(f"{point0[0, 1]:.1f}")
+        hidden1_text.set_text(f"{h1[0]:.2f}")
+        hidden2_text.set_text(f"{h1[1]:.2f}")
+        out1_text.set_text(f"{h2[0]:.2f}")
+        out2_text.set_text(f"{h2[1]:.2f}")
+        out3_text.set_text(f"{h2[2]:.2f}")
 
-        curr_class = np.argmax(out_[0])
+        # Class circle
+        curr_class = np.argmax(h2)
         class_text.set_text(classes[curr_class])
         class_circle.set_facecolor(colors[curr_class])
 
         pbar.update(1)
-        return animated_artists
+        return artists_to_animate
 
-    ax_upperleft.set_xlim(*x_lim)
-    ax_upperleft.set_ylim(*y_lim)
-    ax_upperleft.set_aspect("equal")
-    ax_upperleft.set_xlabel("Weight (g)")
-    ax_upperleft.set_ylabel("Diameter (cm)")
-    ax_upperleft.set_aspect("equal")
-    ax_upperleft.set_title("Lines for apples and pears")
-
-    ax_upperright.set_xlabel("Appleness")
-    ax_upperright.set_ylabel("Pearness")
-    ax_upperright.set_title("Activation space")
-    ax_upperright.set_xlim(*outlims[0])
-    ax_upperright.set_ylim(*outlims[1])
-    ax_upperright.set_aspect("equal")
-
-    ax_bottom.set_aspect("equal")
-    ax_bottom.set_xlim(-3, 3)
-    ax_bottom.set_ylim(-1, 1)
-    ax_bottom.set_xticks([])
-    ax_bottom.set_yticks([])
-    ax_bottom.axis("off")
-
-    fig.tight_layout()
     anim = FuncAnimation(fig, step, blit=True, interval=0, frames=n)
     plt.show()
 
@@ -803,5 +614,5 @@ if __name__ == "__main__":
     # visualize_2lp_activations_animated()
     # visualize_appleness_pearness()
     # visualize_appleness_pearness_out_lines()
-    visualize_3lp()
-    # visualize_3lp_animated()
+    # visualize_3lp()
+    visualize_3lp_animated()
